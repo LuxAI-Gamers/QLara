5917cd388455f5e9b819c1cf48f1c47482d3ccf4
Ángel Delgado <angel.sherpa80@gmail.com>
2021-09-22 00:29:44 +0200

    feat(#6): add inception layer and huber loss
    * feat(#6): add inception layer
    
    * feat(#6): add Hubber loss

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
-import math
+import os
 import sys
+import math
 import random
+from functools import partial
 
 import numpy as np
 import tensorflow as tf
@@ -23,20 +25,15 @@ class QLModel():
 
     def build_network(self, inputs):
 
-        x = tf.keras.layers.Conv2D(self._output_shape,
-                                   (3, 3),
-                                   padding='same',
-                                   activation='relu')(inputs)
+        # Inception
+        x = self._inception_block(self._output_shape)(inputs)
+        x = self._inception_block(self._output_shape)(x)
+        x = self._inception_block(self._output_shape)(x)
+        x = self._inception_block(self._output_shape)(x)
+        x = self._inception_block(self._output_shape)(x)
 
-        x = tf.keras.layers.Conv2D(self._output_shape,
-                                   (3, 3),
-                                   padding='same',
-                                   activation='relu')(x)
-
-        x = tf.keras.layers.Conv2D(self._output_shape,
-                                   (3, 3),
-                                   padding='same',
-                                   activation='relu')(x)
+        # Residual connection
+        x = tf.keras.layers.concatenate([x, inputs], axis=-1)
 
         x = tf.keras.layers.Dense(self._output_shape,
                                   activation='softmax',
@@ -44,9 +41,49 @@ class QLModel():
 
         model = tf.keras.Model(inputs=inputs, outputs=x)
 
-        model.compile(loss='mse', optimizer='adam')
+        model.compile(loss=tf.keras.losses.Huber(),
+                      optimizer='adam')
 
         return model
+
+
+    def _inception_block(self, filters):
+
+        f1 = (filters-1)//3
+        f2 = (filters-1)//3
+        f3 = (filters-1)//3
+        f4 = 1 + (filters-1) % 3
+
+        def inception_function(layer_in, f1, f2, f3, f4):
+            # 1x1 conv
+            conv1 = tf.keras.layers.Conv2D(f1,
+                                         (1,1),
+                                         padding='same',
+                                         activation='relu')(layer_in)
+            # 3x3 conv
+            conv3 = tf.keras.layers.Conv2D(f2,
+                                           (3,3),
+                                           padding='same',
+                                           activation='relu')(layer_in)
+            # 5x5 conv
+            conv5 = tf.keras.layers.Conv2D(f3,
+                                           (5,5),
+                                           padding='same',
+                                           activation='relu')(layer_in)
+            # 3x3 max pooling
+            pool_fun = tf.keras.layers.MaxPooling2D((3,3),
+                                                    strides=(1,1),
+                                                     padding='same')
+
+            pools = [pool_fun(layer_in) for i in range(f4)]
+
+            # concatenate filters, assumes filters/channels last
+            layers = [conv1, conv3, conv5] + pools
+            layer_out = tf.keras.layers.concatenate(layers, axis=-1)
+
+            return layer_out
+
+        return partial(inception_function,f1=f1,f2=f2,f3=f3,f4=f4)
 
 
     def fit(self, env_state, reward):
@@ -63,9 +100,16 @@ class QLModel():
         return self._models[dim].predict(np.asarray([env_state]))[0]
 
 
-    def save(self, model_path):
-        self._model.save(model_path)
+    def save(self, model_dir):
+        for name, model in self._models.items():
+            model.save(model_dir+f'/models_{name}.h5')
 
 
-    def load(self, model_path):
-        self._model = tf.keras.model.load_model(model_path)
+    def load(self, model_dir):
+
+        self._models = {}
+        for file_name in os.listdir(model_dir):
+            if file_name.endswith('.h5'):
+                dim = int(file_name.split('.')[0][-2:])
+                model = tf.keras.model.load_model(model_dir+'/'+file_name)
+                self._models[dim] = model
