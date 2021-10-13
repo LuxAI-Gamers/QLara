7824ca326d1b3f3bce1f7950bfcad0642ee0f5d5
alesolano <asolanopf@gmail.com>
2021-10-13 16:06:51 +0200

    change how hyperparameters are tracked in train.py

--- train.py
+++ train.py
@@ -1,19 +1,30 @@
+import os
 import json
 import random
-import argparse
 from kaggle_environments import make
 from IPython.display import clear_output
 
 from agent import agent, clara
 
+args = {
+    'episodes': 25,
+    'model_dir': './models'
+}
+
 if __name__ == '__main__':
+    ### HYPERPARAMETERS ###
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--episodes', type=int, default=25)
-    parser.add_argument('--model_dir', type=str, default='./models')
-    args = parser.parse_args()
+    if os.path.isfile('/opt/ml/input/config/hyperparameters.json'):
+        # This is where SageMaker puts the hyperparameters
+        # I would love to use argparse, but that's not how SageMakers developers have made it:
+        # https://github.com/aws/sagemaker-training-toolkit/issues/65
+        with open('/opt/ml/input/config/hyperparameters.json', 'r') as f:
+            sagemaker_args = json.load(f)
 
-    for ep in range(args.episodes):
+        args.update(sagemaker_args)
+
+    episodes = int(args["episodes"])
+    for ep in range(episodes):
         clear_output()
         print(f"=== Episode {ep} ===")
         env = make("lux_ai_2021",
@@ -25,7 +36,8 @@ if __name__ == '__main__':
         steps = env.run([agent, "simple_agent"])
     print([step[0]['action'] for step in steps])
 
-    clara._model.save(args.model_dir)
+    clara._model.save(args["model_dir"])
+    print(os.listdir(args["model_dir"]))
 
     replay = env.toJSON()
     with open("replay.json", "w") as f:
