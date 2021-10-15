3d26bfc21f34ed68ec241a4b8fbd2a1a1ec7cdd0
alesolano <asolanopf@gmail.com>
2021-10-15 11:03:43 +0200

    add new configuration style to train.py

--- train.py
+++ train.py
@@ -4,26 +4,62 @@ import random
 from kaggle_environments import make
 from IPython.display import clear_output
 
-from agent import agent, clara
+from lux.game import Game
+from clarai import Clara
 
-args = {
+
+def agent(observation):
+    global game_state, clara
+
+    ### Do not edit ###
+    if observation["step"] == 0:
+        game_state = Game()
+        game_state._initialize(observation["updates"])
+        game_state._update(observation["updates"][2:])
+        game_state.id = observation.player
+        # clara._model.load('models/1632520074/')
+        clara.init_memory(game_state, observation)
+        actions = []
+    else:
+        game_state._update(observation["updates"])
+
+        ### AI Code goes down here! ###
+        actions = clara.play(game_state, observation)
+
+    return actions
+
+
+configuration = {
+    'lr': 0.01,
+    'gamma': 0.95,
+    'epsilon': 0.95,
+    'epsilon_final': 0.01,
+    'epsilon_decay': 0.995,
+    'batch_length': 12,
+    'epochs': 1,
     'episodes': 25,
     'model_dir': './models'
 }
 
-if __name__ == '__main__':
-    ### HYPERPARAMETERS ###
 
+if __name__ == '__main__':
+    # Read hyperparameters from Sagemaker if applicable
     if os.path.isfile('/opt/ml/input/config/hyperparameters.json'):
-        # This is where SageMaker puts the hyperparameters
-        # I would love to use argparse, but that's not how SageMakers developers have made it:
-        # https://github.com/aws/sagemaker-training-toolkit/issues/65
         with open('/opt/ml/input/config/hyperparameters.json', 'r') as f:
             sagemaker_args = json.load(f)
 
-        args.update(sagemaker_args)
+        configuration.update(sagemaker_args)
 
-    episodes = int(args["episodes"])
+    print(configuration)
+
+    # Extract external parameters from Clarai configuration
+    episodes = int(configuration.pop('episodes'))
+    model_dir = configuration.pop('model_dir')
+
+    # Create Clarai agent with configuration
+    clara = Clara(**configuration)
+
+    # Play and learn
     for ep in range(episodes):
         clear_output()
         print(f"=== Episode {ep} ===")
@@ -42,8 +78,8 @@ if __name__ == '__main__':
         print(
             f'total_rounds={total_rounds}; units_created={units_created}; cities_created={cities_created};')
 
-    clara._model.save(args["model_dir"])
-    print(os.listdir(args["model_dir"]))
+    # Save model
+    clara._model.save(model_dir)
 
     replay = env.toJSON()
     with open("replay.json", "w") as f:
